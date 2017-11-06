0964cb6b7aa1c8a1c33f18d833252bd1efcc3706
nicrausaz <n.crausaz99@gmail.com>
2017-11-06 15:07:11 +0100

    link working

--- index.js
+++ index.js
@@ -6,10 +6,16 @@
 // @author       nicrausaz
 // @match        https://gchotline.epfl.ch/*
 // @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
+$(document).ready(function () {
+    let infos = ($('.tooltip-content p span')[0].innerText).split(' ');
+    let url = 'https://people.epfl.ch/' + infos[0];
+    let name = $('.article-header span')[0].innerText;
 
-    // Your code here...
-})();
+    $('.article-header span').each( function () {
+        console.log(this);
+        this.innerHTML = `<a href=${url}>${name}</a>`;
+    });
+});
