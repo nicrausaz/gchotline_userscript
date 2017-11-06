e9189e3bb4786fe33d2df7aeb27ac91615fd2a62
nicrausaz <n.crausaz99@gmail.com>
2017-11-06 14:03:48 +0100

    base

---
+++ index.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         GC_Hotline_Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Link to people.epfl.ch on user name
+// @author       nicrausaz
+// @match        https://gchotline.epfl.ch/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
